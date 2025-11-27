bba7b073128319faf59575e54fe6959dc301b560
Valentina Ramírez <anaj0111719@gmail.com>
2025-11-27 12:41:40 -0500

    [UPDATING] Moving portfolio to /portfolio/

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
+import react from "@astrojs/react";
 
 export default defineConfig({
-  site: "https://luminaw.co",
-  base: "/",
-  integrations: [tailwind(), mdx()],
+	site: "https://luminaw.co",
+	base: "/",
+	integrations: [tailwind(), mdx(), react()],
 });
