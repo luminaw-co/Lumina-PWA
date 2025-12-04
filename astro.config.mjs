b27db10b29e91abcd7e524423567c18a63401215
Valentina Ramírez <anaj0111719@gmail.com>
2025-12-04 10:04:15 -0500

    [UPDATING] Routes in computer

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,12 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
+import netlify from "@astrojs/netlify/functions";
 
 export default defineConfig({
 	site: "https://luminaw.co",
 	base: "/",
 	integrations: [tailwind(), mdx(), react()],
+	output: "server",
+	adapter: netlify(),
 });
